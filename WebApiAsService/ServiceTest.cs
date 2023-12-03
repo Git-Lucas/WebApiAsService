e0256e1c59cd9e471fc708b560ddacaffd64f612
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2023-12-03 18:18:56 -0300

    Add project files.

---
+++ WebApiAsService/ServiceTest.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApiAsService;
+
+public class ServiceTest : BackgroundService
+{
+    public ServiceTest(ILoggerFactory loggerFactory)
+    {
+        Logger = loggerFactory.CreateLogger<ServiceTest>();
+    }
+
+    public ILogger Logger { get; }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Logger.LogInformation("ServiceTest is starting.");
+
+        stoppingToken.Register(() => Logger.LogInformation("ServiceTest is stopping."));
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Logger.LogInformation("ServiceTest is doing background work.");
+
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+        }
+
+        Logger.LogInformation("ServiceTest has stopped.");
+    }
+}
